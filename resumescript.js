cd017434584d8d8b17a1bcc894e3fcbc104cc5bc
shanvicky5 <127470178+shanvicky5@users.noreply.github.com>
2023-04-26 17:07:10 +0530

    Add files via upload

---
+++ resumescript.js
@@ -0,0 +1,44 @@
+let Ownresume={
+    "basics":
+        {
+        "name": "vignesh Murugesan",
+        "email": "shanvicky5@gmail.com",
+        "mobile": "9994558278",
+        "degree" : " Msc",
+        "address": {
+            "street":" 1771 thanipadi main Road",
+            "village": " Kilravandavadi",
+            "taluk": " Thandarampattu",
+            "District": "Thiruvannamalai",
+            "pin code" : "606707"
+        },
+        },
+        "Ojective": [
+            {
+            "objective": "i would like to out the software professional where i can work as team member tosecure position where i can efficiently contribute my skill to growth career"
+        },
+    ],
+        "education" : [
+            {
+            "collage" : "loyola collage",
+            "department" : "Computer science",
+            "pass out" :  "2017",
+            "percentage" : "73.8"
+
+        },
+    ],
+    "skills" : [
+        {
+            "front end" : " html, css, javascript",
+            "back end " : " java, python",
+            "database" : " mysql, ms sql"
+        },
+    ],
+    "languages" : [
+        {
+            "speak" : " english, tamil, kannada",
+            "written" :  " english, tamil",
+        }
+    ]
+}
+console.log(Ownresume);
